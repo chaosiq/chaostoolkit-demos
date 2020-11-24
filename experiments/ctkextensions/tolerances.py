17ada0062c4cd123114e418279625cdcf7eb807d
Sylvain Hellegouarch <sh@defuze.org>
2020-11-24 14:47:43 +0100

    Add prometheus experiment
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- experiments/ctkextensions/tolerances.py
+++ experiments/ctkextensions/tolerances.py
@@ -1,8 +1,10 @@
 import os.path
+from typing import Any, Dict
 
+from chaoslib.exceptions import ActivityFailed
 from logzero import logger
 
-__all__ = ["should_not_have_any_errors"]
+__all__ = ["should_not_have_any_errors", "error_count_should_not_grow"]
 
 
 def should_not_have_any_errors(filepath: str) -> bool:
@@ -18,4 +20,20 @@ def should_not_have_any_errors(filepath: str) -> bool:
             if "error" in l:
                 logger.error("Found an error in traces")
                 return False
+    return True
+
+
+def error_count_should_not_grow(value: Dict[str, Any] = None) -> bool:
+    """
+    Go through all the error counts and raise an error when we notice an
+    increase.
+    """
+    values = value["data"]["result"][0]["values"]
+    if not values:
+        return True
+
+    values = set(values[1:len(values):2])
+    if len(values) > 1:
+        raise ActivityFailed("Error counts: {}".format(values))
+
     return True
